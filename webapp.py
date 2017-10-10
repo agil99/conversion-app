f2cf1e229a21e4e9d0c5cf3d8a0724d253dca480
jaredfitton <jaredfitton@gmail.com>
2017-10-10 09:40:28 -0700

    Try to fix burritos

--- webapp.py
+++ webapp.py
@@ -9,11 +9,13 @@ def render_main():
 @app.route("/response")
 def render_response():
     money = float(request.args['money'])
+    print money
     # The request object stores information about the request sent to the server.
     # args is a MultiDict (like a dictionary but can have multiple values for the same keys
     # The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
     num_burritos = money/8.5
-    return render_template('burrito_response.html', num_burritos)
+    print num_burritos
+    return render_template('burrito_response.html', response = num_burritos)
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
