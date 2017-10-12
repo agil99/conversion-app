d89382b4992ca2530aa247d19b85bacec1a7e638
jaredfitton <jaredfitton@gmail.com>
2017-10-12 09:43:36 -0700

    Fix meter conversion

--- webapp.py
+++ webapp.py
@@ -6,23 +6,23 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('index.html')
 
-# @app.route("/burrito")
-# def render_burrito():
-#     money = float(request.args['money'])
-#     # The request object stores information about the request sent to the server.
-#     # args is a MultiDict (like a dictionary but can have multiple values for the same keys
-#     # The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
-#     num_burritos = roundFloat(money/8.5)
-#     return render_template('burrito.html', response = "test")
+@app.route("/burrito")
+def render_burrito():
+    # money = float(request.args['money'])
+    # # The request object stores information about the request sent to the server.
+    # # args is a MultiDict (like a dictionary but can have multiple values for the same keys
+    # # The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
+    # num_burritos = roundFloat(money/8.5)
+    return render_template('burrito.html', response = "test")
 
-# @app.route("/meter")
-# def render_meter():
-#     if "meters" in request.args:
-#         meters = request.args['meters']
-#         centimeters = roundFloat(meters*100)
-#         return render_template('meter_to_centimeter.html', response = centimeters)
-#     else:
-#         return render_template('meter_to_centimeter.html' response = "Enter")
+@app.route("/meter")
+def render_meter():
+    if 'meters' in request.args:
+        meters = request.args['meters']
+        centimeters = roundFloat(meters*100)
+        return render_template('meter_to_centimeter.html', response = centimeters)
+    else:
+        return render_template('meter_to_centimeter.html' response = "Enter")
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
