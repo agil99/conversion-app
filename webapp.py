a8e04668f543b90b0a707893351495d3d8afed3a
jaredfitton <jaredfitton@gmail.com>
2017-10-12 09:51:54 -0700

    Fix meter conversion

--- webapp.py
+++ webapp.py
@@ -8,12 +8,12 @@ def render_main():
 
 @app.route("/burrito")
 def render_burrito():
-    # money = float(request.args['money'])
-    # # The request object stores information about the request sent to the server.
-    # # args is a MultiDict (like a dictionary but can have multiple values for the same keys
-    # # The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
-    # num_burritos = roundFloat(money/8.5)
-    return render_template('burrito.html', response = "test")
+    money = float(request.args['money'])
+    # The request object stores information about the request sent to the server.
+    # args is a MultiDict (like a dictionary but can have multiple values for the same keys
+    # The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
+    num_burritos = roundFloat(money/8.5)
+    return render_template('burrito.html', response = num_burritos)
 
 @app.route("/meter")
 def render_meter():
